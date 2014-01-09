48b9e5735367d7c04cfdcee7999d43d4523597b5
hg <hg@localhost>
2014-01-10 00:59:23 +0100

    automatic creation of line number

--- de.bayen.freibier/src/de/bayen/freibier/model/MBAYInterestCalculationLine.java
+++ de.bayen.freibier/src/de/bayen/freibier/model/MBAYInterestCalculationLine.java
@@ -3,6 +3,8 @@ package de.bayen.freibier.model;
 import java.sql.ResultSet;
 import java.util.Properties;
 
+import org.compiere.util.DB;
+
 public class MBAYInterestCalculationLine extends X_BAY_InterestCalculationLine {
 
 	private static final long serialVersionUID = 1L;
@@ -16,4 +18,19 @@ public class MBAYInterestCalculationLine extends X_BAY_InterestCalculationLine {
 			String trxName) {
 		super(ctx, rs, trxName);
 	}
+
+	@Override
+	protected boolean beforeSave(boolean newRecord) {
+		// Get Line No
+		if (getLine() == 0) {
+			String sql = "SELECT COALESCE(MAX(Line),0)+10 FROM " + Table_Name
+					+ " WHERE " + COLUMNNAME_BAY_InterestCalculation_ID + "=?";
+			int ii = DB.getSQLValue(get_TrxName(), sql,
+					getBAY_InterestCalculation_ID());
+			setLine(ii);
+		}
+
+		return super.beforeSave(newRecord);
+	}
+
 }
