633e9296e9e7151bcadf6040082e0e04fff49e76
hg <hg@localhost>
2014-01-10 16:57:57 +0100

    ProcessFactory implementiert, um die Extensions damit in Zukunft abzul?sen

---
+++ de.bayen.freibier/src/de/bayen/freibier/ProcessFactory.java
@@ -0,0 +1,21 @@
+package de.bayen.freibier;
+
+import org.adempiere.base.IProcessFactory;
+import org.compiere.process.ProcessCall;
+
+public class ProcessFactory implements IProcessFactory {
+
+	/**
+	 * Bisher habe ich alle meine Prozesse mit einer Extension gemacht. Das ist
+	 * aber inzwischen uncool geworden. Außerdem mag ich es auch lieber, meine
+	 * Zuordnungen durch debugbaren Java-Code laufen zu lassen anstatt durch die
+	 * Magie der OSGi-Extensions. Also habe ich diese Klasse hier schonmal
+	 * vorbereitet. Beim nächsten Prozess nehme ich die und wenn alles glatt
+	 * läuft, können die vorhandenen Prozeß-Extensions dann umgestellt werden.
+	 */
+	@Override
+	public ProcessCall newProcessInstance(String className) {
+		return null;
+	}
+
+}
