b2285b6129f14a3c6c2f5aaf26b7873e68c9965c
tbayen <tbayen@localhost>
2014-08-25 11:50:04 +0200

    there was no activator to activate the 2Pack file

---
+++ de.bayen.bx.lde/src/de/bayen/bx/lde/Activator.java
@@ -0,0 +1,9 @@
+package de.bayen.bx.lde;
+
+import org.osgi.framework.BundleActivator;
+
+import de.bayen.bx.util.Incremental2PackActivator;
+
+public class Activator extends Incremental2PackActivator implements
+		BundleActivator {
+}
