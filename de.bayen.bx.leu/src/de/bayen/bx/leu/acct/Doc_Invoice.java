72b58207b6f4f7480ce2336687de41b4d8d5b265
tbayen <tbayen@localhost>
2014-09-13 20:57:16 +0200

    Posting manipulation only for sales orders

--- de.bayen.bx.leu/src/de/bayen/bx/leu/acct/Doc_Invoice.java
+++ de.bayen.bx.leu/src/de/bayen/bx/leu/acct/Doc_Invoice.java
@@ -11,6 +11,7 @@ import org.compiere.acct.FactLine;
 import org.compiere.model.I_C_Tax_Acct;
 import org.compiere.model.MAccount;
 import org.compiere.model.MAcctSchema;
+import org.compiere.model.MInvoice;
 import org.compiere.model.X_C_Tax_Acct;
 import org.compiere.util.CPreparedStatement;
 import org.compiere.util.DB;
@@ -33,44 +34,47 @@ public class Doc_Invoice extends org.compiere.acct.Doc_Invoice {
 		for (Fact fact : facts) {
 			FactLine[] factLines = fact.getLines();
 			for (FactLine factLine : factLines) {
-				DocLine docLine = factLine.getDocLine();
-				if (docLine != null && docLine.getM_Product_ID() > 0
-						&& docLine.getC_Tax_ID() > 0) {
-					String sql = "SELECT * FROM " + I_C_Tax_Acct.Table_Name
-							+ " WHERE " + I_C_Tax_Acct.COLUMNNAME_C_Tax_ID
-							+ "=? " + " AND "
-							+ I_C_Tax_Acct.COLUMNNAME_C_AcctSchema_ID + "=? ";
-					CPreparedStatement pstm = DB.prepareStatement(sql,
-							getTrxName());
-					X_C_Tax_Acct taxAcct = null;
-					try {
-						pstm.setInt(1, docLine.getC_Tax_ID());
-						pstm.setInt(2, as.get_ID());
-						ResultSet rs = pstm.executeQuery();
-						if (rs.next())
-							taxAcct = new X_C_Tax_Acct(getCtx(), rs,
-									getTrxName());
-						if (rs.next())
-							throw new AdempiereException(
-									"this does not work with more than one tax accounting record");
-					} catch (SQLException e) {
-						throw new AdempiereException(e);
-					} finally {
+				if (((MInvoice) getPO()).isSOTrx()) {
+					DocLine docLine = factLine.getDocLine();
+					if (docLine != null && docLine.getM_Product_ID() > 0
+							&& docLine.getC_Tax_ID() > 0) {
+						String sql = "SELECT * FROM " + I_C_Tax_Acct.Table_Name
+								+ " WHERE " + I_C_Tax_Acct.COLUMNNAME_C_Tax_ID
+								+ "=? " + " AND "
+								+ I_C_Tax_Acct.COLUMNNAME_C_AcctSchema_ID
+								+ "=? ";
+						CPreparedStatement pstm = DB.prepareStatement(sql,
+								getTrxName());
+						X_C_Tax_Acct taxAcct = null;
 						try {
-							if (pstm != null)
-								pstm.close();
+							pstm.setInt(1, docLine.getC_Tax_ID());
+							pstm.setInt(2, as.get_ID());
+							ResultSet rs = pstm.executeQuery();
+							if (rs.next())
+								taxAcct = new X_C_Tax_Acct(getCtx(), rs,
+										getTrxName());
+							if (rs.next())
+								throw new AdempiereException(
+										"this does not work with more than one tax accounting record");
 						} catch (SQLException e) {
 							throw new AdempiereException(e);
+						} finally {
+							try {
+								if (pstm != null)
+									pstm.close();
+							} catch (SQLException e) {
+								throw new AdempiereException(e);
+							}
+							pstm = null;
 						}
-						pstm = null;
+						int revenueAcctCombi_ID = taxAcct
+								.get_ValueAsInt(COLUMNNAME_P_REVENUE_ACCT);
+						// is RevenueAccountingByTax configured?
+						if (revenueAcctCombi_ID <= 0)
+							continue;
+						factLine.setAccount(as, new MAccount(getCtx(),
+								revenueAcctCombi_ID, getTrxName()));
 					}
-					int revenueAcctCombi_ID = taxAcct
-							.get_ValueAsInt(COLUMNNAME_P_REVENUE_ACCT);
-					// is RevenueAccountingByTax configured?
-					if (revenueAcctCombi_ID <= 0)
-						continue;
-					factLine.setAccount(as, new MAccount(getCtx(),
-							revenueAcctCombi_ID, getTrxName()));
 				}
 			}
 		}
