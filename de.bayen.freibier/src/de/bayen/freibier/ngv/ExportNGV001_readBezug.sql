456e9a0556c0551c699604f6472c8632dba83849
Carlos Ruiz <carg67@gmail.com>
2025-01-09 18:36:50 +0100

    Support #10393 Bug on NGV exporter - duplicating lines

--- de.bayen.freibier/src/de/bayen/freibier/ngv/ExportNGV001_readBezug.sql
+++ de.bayen.freibier/src/de/bayen/freibier/ngv/ExportNGV001_readBezug.sql
@@ -55,11 +55,14 @@ LEFT JOIN bay_umsatzstatistik ON (bay_umsatzstatistik.bay_statistikperiode_id = 
 LEFT JOIN C_BPartner ON (C_BPartner.C_BPartner_ID = BAY_Umsatzstatistik.C_BPartner_ID)
 LEFT JOIN M_Product ON (M_Product.M_Product_ID = BAY_Umsatzstatistik.m_product_id)
 LEFT JOIN BAY_TradingUnit ON (M_Product.BAY_TradingUnit_ID = BAY_TradingUnit.bay_tradingunit_id)
-LEFT JOIN c_bpartner_location ON(
-  c_bpartner.c_bpartner_id=c_bpartner_location.c_bpartner_id 
-  AND c_bpartner_location.isbillto='Y'  /* Rechnungsadresse */
-  AND c_bpartner_location.isactive='Y'
-  )
+LEFT JOIN C_BPartner_Location ON (
+  C_BPartner_Location.C_BPartner_Location_ID = (
+    SELECT C_BPartner_Location_ID
+    FROM C_BPartner_Location bpl
+    WHERE bpl.isActive='Y' AND C_BPartner.C_BPartner_ID=bpl.C_BPartner_ID
+    ORDER BY bpl.IsShipTo DESC, bpl.C_BPartner_Location_ID
+    LIMIT 1
+  ))
 LEFT JOIN c_location ON (c_location.c_location_id = c_bpartner_location.c_location_id)
 LEFT JOIN c_country ON (c_country.c_country_id = c_location.c_country_id)
 WHERE bay_statistikperiode.bay_statistikperiode_id=?
